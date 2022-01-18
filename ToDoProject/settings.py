9010724e2ac607a95a85c2a67de01b109a65f372
neerajadhav <adhavneeraj9500@gmail.com>
2022-01-18 14:25:02 +0530

    setting

--- ToDoProject/settings.py
+++ ToDoProject/settings.py
@@ -116,7 +116,7 @@ LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
